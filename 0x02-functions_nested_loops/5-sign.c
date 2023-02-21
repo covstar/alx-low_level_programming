746861ec81ec6c87daf14cb7083e35937629b366
covstar <okechukwuvictor093@gmail.com>
2023-02-21 21:07:04 +0000

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,23 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
-	}
-	else
-	{
-		putchar('-');
-		return (-1);
-	}
-	return (n);
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+putchar('-');
+return (-1);
+}
+else
+{
+putchar('-');
+return ('/');
+}
 }
