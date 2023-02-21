b671a69d038008dd10d8003c332393bbce3a93b0
covstar <okechukwuvictor093@gmail.com>
2023-02-21 20:56:17 +0000

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,23 +6,27 @@
 * Description: printing positive, negative, or zero
 * Return: 1 and prints + if n > 0, ret 0/print0 if n = 0, ret -1/prnt- in n < 0
 */
-nt print_sign(int n)
-{
-if (n > 0)
-{
-printf('+');
-return (1);
-}
-else if (n == 0)
-{
-printf('0');
-return (0);
-}
-else if (n < 0)
-{
-printf('-');
-return (-1);
-}
+int print_sign(int n)
+
+    {
+	if (n > 0)
+	{
+		printf('+');
+		return (1);
+	}
+	
+	else if (n == 0)
+     {
+
+		printf('0');
+		return (0);
+     }
+	else if (n < 0)
+     {
+
+		printf('-');
+		return (-1);
+     }
 else
 {
 printf('-');
