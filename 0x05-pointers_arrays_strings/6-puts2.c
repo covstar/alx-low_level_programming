4bc4604e026150469789f9c8f246226d71d4141e
covstar <okechukwuvictor093@gmail.com>
2023-02-28 23:11:46 +0000

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,30 +1,20 @@
 #include "main.h"
+
 /**
-* puts2 - function
-* @str: string operand
-*
-* Description: print every other char of a string starting with 1stw/ \n
-* Return: string
-*/
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
 void puts2(char *str)
 {
-	int longi = 0;
-	int i = 0;
-	char *y = str;
-	int o;
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
 
-	while (*y != '\0')
-	{
-		longi++;
-		y++;
-	}
-	i = longi - 1;
-	for (o = 0; o < i; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
+_putchar('\n');
 }
