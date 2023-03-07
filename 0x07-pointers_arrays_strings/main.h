558d83644e389d1e4c1a367a59ad5c2f69116a6c
covstar <okechukwuvictor093@gmail.com>
2023-03-07 09:44:40 +0000

    Task 9

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * File: main.h
- * Auth: sam tech (samuel birhanu)
- * description: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
 #include <stddef.h>
-
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
+#include <stdio.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
+unsigned int _strspn(char *s, char *accept);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+#endif /* MAIN_H */
