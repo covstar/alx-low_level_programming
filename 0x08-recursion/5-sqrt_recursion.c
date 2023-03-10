8e47e90cc31a59e495e63cf04b2c825c5b1d704b
covstar <okechukwuvictor093@gmail.com>
2023-03-10 02:49:50 +0000

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,29 @@
 #include "main.h"
 /**
-* _pow_recursion - function that returns the
-* value of x raised to the power of y
-*@x: the base
-*@y: the exponent
-*Return: the value of x raised to the power of y
+*find_sqrt_recursion - finds the square root of number
+*@i: parameter for number to finds its squre root
+*@j: parameter for returning the squre root of a number
+*Return: 1, -1 or the square root
 */
-int _pow_recursion(int x, int y)
+
+int find_sqrt_recursion(int i, int j)
 {
-		if (y < 0)
-			return (-1);
-		else if (y == 0)
-			return (1);
-		return (_pow_recursion(x, y - 1) * x);
+if (j * j == i)
+return (j);
+
+if (j * j > i)
+return (-1);
+return (find_sqrt_recursion(i, j + 1));
+}
+
+/**
+*_sqrt_recursion - calls square root function
+*Allows passing of only the number to find its square root
+*@n: parameter to pass the number to finds its square root
+*Return: 1, -1 or the square root
+*/
+
+int _sqrt_recursion(int n)
+{
+return (find_sqrt_recursion(n, 1));
 }
