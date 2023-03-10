9bddb10e5233bebf153caad911ba0c4548cf0024
covstar <okechukwuvictor093@gmail.com>
2023-03-10 03:10:08 +0000

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,15 @@
 * @s: string to be measured
 *Return: length of the string
 */
-int _strlen_recursion(char *s) 
+
+int _strlen_recursion(char *s)
 {
-int len = 0;
-if (*s != '\0') 
-{
-len++;
-len += _strlen_recursion(s + 1);
-}
-return (len);
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }
