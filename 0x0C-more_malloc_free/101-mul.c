e6cab0447a5c391f36b8b4f8f3565115b41f58bd
covstar <okechukwuvictor093@gmail.com>
2023-03-17 19:54:51 +0000

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,88 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
-int main(int argc, char *argv[])
+#include <ctype.h>
+#include <string.h>
+
+void print_error(void)
 {
-unsigned long mul;
-int i, j;
-if (argc != 3)
-{
-printf("Error\n");
-exit(98); }
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{
-printf("Error\n");
-exit(98);
-}
+    printf("Error\n");
+    exit(98);
 }
 
+void check_digits(char *num)
+{
+    for (int i = 0; i < strlen(num); i++)
+    {
+        if (!isdigit(num[i]))
+        {
+            print_error();
+        }
+    }
 }
-mul = atol(argv[1]) *  atol(argv[2]);
-printf("%lu\n", mul);
-return (0);
+
+int multiply(char *num1, char *num2, char *result)
+{
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+
+    int *res = calloc(len1 + len2, sizeof(int));
+
+    for (int i = len1 - 1; i >= 0; i--)
+    {
+        for (int j = len2 - 1; j >= 0; j--)
+        {
+            int x = (num1[i] - '0') * (num2[j] - '0');
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = x + res[p2];
+
+            res[p1] += sum / 10;
+            res[p2] = sum % 10;
+        }
+    }
+
+    int i = 0;
+    while (i < len1 + len2 && res[i] == 0)
+    {
+        i++;
+    }
+
+    if (i == len1 + len2)
+    {
+        result[0] = '0';
+        result[1] = '\0';
+    }
+    else
+    {
+        int j = 0;
+        while (i < len1 + len2)
+        {
+            result[j] = res[i] + '0';
+            i++;
+            j++;
+        }
+        result[j] = '\0';
+    }
+
+    free(res);
+    return (0);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        print_error();
+    }
+
+    check_digits(argv[1]);
+    check_digits(argv[2]);
+
+    char *result = malloc(strlen(argv[1]) + strlen(argv[2]) + 1);
+
+    multiply(argv[1], argv[2], result);
+
+    printf("%s\n", result);
+
+    free(result);
+    return (0);
 }
