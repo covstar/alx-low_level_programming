0ccdee38c443e0230befe3f761f63a09bea1d95c
covstar <okechukwuvictor093@gmail.com>
2023-04-04 13:36:50 +0000

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,31 +3,25 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head of the list
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
  *
- * Return: The number of nodes in the list
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;  /* Initialize the node count to zero */
-    const listint_t *temp = head;  /* Create a temporary pointer to the head */
+	size_t count = 0;
 
-    while (temp != NULL)
-    {
-        printf("[%p] %d\n", (void *)temp, temp->n);  /* Print the node address and value */
-        count++;  /* Increment the node count */
-
-        /* Check if the next node has already been printed */
-        if (temp <= temp->next)
-        {
-            printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
-            printf("-> [%p] %d\n", (void *)temp->next->next, temp->next->next->n);
-            exit(98);  /* Exit the program with status 98 */
-        }
-
-        temp = temp->next;  /* Move to the next node */
-    }
-
-    return (count);  /* Return the node count */
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		if (head <= head->next)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+		head = head->next;
+	}
+	return (count);
 }
